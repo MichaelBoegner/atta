2775a3f3b946b82bdcd475e5a575d8c7e3f0dfa9
michaelboegner <cmikeb@gmail.com>
2024-05-26 14:57:48 -0400

    fixed def name. Added condition to return slack's challenge only if the key 'challenge' exists. Next step is to verify request being sent is from slack. Then post to sql server.

--- server.py
+++ server.py
@@ -2,6 +2,10 @@ from flask import Flask, request, make_response
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
-def hello_world():
-    if request.method == 'POST':
-        return request.json['challenge']
+def event_watcher():
+    print("RECEIVED EVENT. REQUEST:",request.json, "END RECEIVED JSON DATA------------")
+    if request.json.get('challenge'):
+        resp = request.json.get('challenge')
+    else: 
+        resp = "great!"
+    return resp
