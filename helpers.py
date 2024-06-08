5b25dd295903a040f1a3f55cdef14717a4f1b432
michaelboegner <cmikeb@gmail.com>
2024-06-08 09:17:27 -0400

    refactored

--- helpers.py
+++ helpers.py
@@ -4,13 +4,8 @@ import psycopg2
 
 def start_db_connection():
     conn_string = "host='localhost' dbname='test' user='michaelboegner' password=''"
-	# print the connection string we will use to connect
     print("Connecting to database\n	->%s %", conn_string)
-
-	# get a connection, if a connect cannot be made an exception will be raised here
     conn = psycopg2.connect(conn_string)
-
-	# conn.cursor will return a cursor object, you can use this cursor to perform queries
     cursor = conn.cursor()
     return cursor
 
